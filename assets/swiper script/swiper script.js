7c6068edc4bcfef62e8b6d5d251d4d434a40ce70
Abdullah801953 <ak786shariq@gmail.com>
2025-01-06 22:01:23 +0530

    mehta-new site commit

---
+++ assets/swiper script/swiper script.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 6,
+    spaceBetween: 70,
+    autoplay: {
+      delay: 3000, // Time between slides (in milliseconds)
+      disableOnInteraction: false, // Continue autoplay after user interaction
+    },
+    breakpoints: {
+      // When window width is >= 768px
+      768: {
+        slidesPerView: 5, // Show 4 slides
+      },
+      // When window width is >= 480px
+      480: {
+        slidesPerView: 3, // Show 2 slides
+      },
+      // When window width is < 480px
+      0: {
+        slidesPerView: 1, // Show 1 slide
+      },
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      enabled: false,
+    },
+  });
+  
---
+++ script.js
@@ -0,0 +1,46 @@
+// sticky navbar
+
+// youtube video
+document.getElementById("play-btn").addEventListener("click", () => {
+    const frame = getElementById("frame2");
+    const videoUrl =
+      "https://www.youtube.com/embed/OTJX8yjiIMs?si=orfXw_BnaZoVDdI-";
+    frame.src = videoUrl;
+    frame.style.display = "block";
+    // document.getElementById("play-btn").style.display = "none";
+  });
+  // ham close and open
+  const coutBtn = document.querySelector(".cout-icon-width");
+  coutBtn.addEventListener("click", () => {
+    const sideNav = document.querySelector(".ham-overlay");
+    sideNav.classList.remove("open-nav");
+  });
+  const menuBtn = document.querySelector(".menu-btn");
+  menuBtn.addEventListener("click", () => {
+    const sideNav = document.querySelector(".ham-overlay");
+    sideNav.classList.add("open-nav");
+  });
+  // counter
+  const counters = document.querySelectorAll('.value');
+  const speed = 200;
+  
+  counters.forEach( counter => {
+     const animate = () => {
+        const value = +counter.getAttribute('data');
+        const data = +counter.innerText;
+       
+        const time = value / speed;
+       if(data < value) {
+            counter.innerText = Math.ceil(data + time);
+            setTimeout(animate, 1);
+          }else{
+            counter.innerText = value;
+          }
+       
+     }
+     
+     animate();
+  });
+  
+  
+  
